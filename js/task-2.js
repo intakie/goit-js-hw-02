3df00f45354c35efd86e0a166e39d2bae278754e
intakie <intakiea@gmail.com>
2024-01-21 14:23:19 +0200

    add js

---
+++ js/task-2.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function formatMessage(message, maxLength) {
+  if (maxLength >= message.length) {
+    return message;
+  } else {
+    const messageCut = message.slice(0, maxLength);
+    return `${messageCut}...`;
+  }
+}
+
+console.log(formatMessage('Curabitur ligula sapien', 16));
+console.log(formatMessage('Curabitur ligula sapien', 23));
+console.log(formatMessage('Vestibulum facilisis purus nec', 20));
+console.log(formatMessage('Vestibulum facilisis purus nec', 30));
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15));
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41));
